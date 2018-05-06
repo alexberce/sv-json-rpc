f21ae41441fc8b6e413ef24cf4d8979300a384c2
Alexandru Berce <alexandru.berce@gmail.com>
2018-05-06 21:38:48 +0300

    Adding new MySQL dump and some documentation

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# sv-json-rpc
+# SmartValue Practical test 
 
-PHP Server
+## Info
+
+
+## Prerequisites
+
+* PHP 5.6 (CLI)
+* composer
+
+## Setup 
+
+1. Create a .env file in the root directory having the variables from .env.example
+2. Run `composer install` to get the required dependencies
+3. Start the php servers using the scripts from the composer file
+
+## Scripts
+
+PHP Servers
 
 ```
     composer run web --timeout=0
     composer run client --timeout=0
     composer run server --timeout=0
 ```
+
+## Servers
+
+[http://127.0.0.1:8080](http://127.0.0.1:8080) - Web Application
+[http://127.0.0.1:8081](http://127.0.0.1:8081) - JSON-RPC client
+[http://127.0.0.1:8082](http://127.0.0.1:8082) - JSON-RPC server
+
+## Tests
--- data/locations_countries.sql
+++ data/locations_countries.sql
@@ -1,24 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2
--- http://www.phpmyadmin.net
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 12, 2013 at 05:18 PM
--- Server version: 5.5.25a
--- PHP Version: 5.3.23
+-- Host: 127.0.0.1
+-- Generation Time: May 06, 2018 at 08:32 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.35
 
-SET FOREIGN_KEY_CHECKS=0;
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `test`
+-- Database: `smart_value`
 --
 
 -- --------------------------------------------------------
@@ -273,7 +274,29 @@ INSERT INTO `locations_countries` (`id`, `name`, `code`, `prefix`) VALUES
 (231, 'Yemen', 'YE', '+967'),
 (232, 'Zambia', 'ZM', '+260'),
 (233, 'Zimbabwe', 'ZW', '+263');
-SET FOREIGN_KEY_CHECKS=1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `locations_countries`
+--
+ALTER TABLE `locations_countries`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `code` (`code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `locations_countries`
+--
+ALTER TABLE `locations_countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=234;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
